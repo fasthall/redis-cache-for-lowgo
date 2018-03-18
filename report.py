d9d0be6bc0c490b5f57ae08ea72e258a2be79bb9
Wei-Tsung Lin <fasthall@gmail.com>
2018-03-18 13:58:05 -0700

    fix url

--- report.py
+++ report.py
@@ -6,5 +6,5 @@ with open('/config/config.yaml', 'r') as ifile:
     obj = yaml.load(ifile)
     print('Reporting to ' + obj['controller'])
     ip = socket.gethostbyname(socket.gethostname())
-    r = requests.post('http://' + obj['controller'] + '/addRedisCache?host=' + ip + ':6379')
+    r = requests.post('http://' + obj['controller'] + '/rediscache?host=' + ip + ':6379')
     print(r.text)
