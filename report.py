d26eef1c9034450272041410804da4e464dd80dc
Wei-Tsung Lin <fasthall@gmail.com>
2018-03-18 17:14:00 -0700

    add retrying loop to report.py

--- report.py
+++ report.py
@@ -6,5 +6,11 @@ with open('/config/config.yaml', 'r') as ifile:
     obj = yaml.load(ifile)
     print('Reporting to ' + obj['controller'])
     ip = socket.gethostbyname(socket.gethostname())
-    r = requests.post('http://' + obj['controller'] + '/rediscache?host=' + ip + ':6379')
-    print(r.text)
+    done = False
+    while not done:
+        try:
+            r = requests.post('http://' + obj['controller'] + '/rediscache?host=' + ip + ':6379')
+            print(r.text)
+            done = True
+        except:
+            time.sleep(2)
